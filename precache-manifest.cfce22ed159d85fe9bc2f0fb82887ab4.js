ed19b8200caf30a81cc02400cf9b85c6e32ff1f0
Harshit Shah <harshitshah4@gmail.com>
2020-05-29 03:22:36 +0530

    Updates

---
+++ precache-manifest.cfce22ed159d85fe9bc2f0fb82887ab4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4522a46b288a12a73528ca2b4c44910b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b2fe54b8b5264bb87fa",
+    "url": "/static/css/main.556d1578.chunk.css"
+  },
+  {
+    "revision": "b69b1b771a6ffdd84f79",
+    "url": "/static/js/2.5ee101a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5ee101a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b2fe54b8b5264bb87fa",
+    "url": "/static/js/main.41936533.chunk.js"
+  },
+  {
+    "revision": "99464e24ba6a5135df00",
+    "url": "/static/js/runtime-main.26f9b5af.js"
+  }
+]);
